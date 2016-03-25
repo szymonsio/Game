534120c0e565df8170f97f19115da1a04cbd0596
MichalPerdek <michal.perdek@itsilesia.com>
2016-03-25 20:39:23 +0100

    Fix some scenes

---
+++ Game/Assets/Scripts/Options.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Options : MonoBehaviour {
+
+    //pause menu appear when esc key is pressed
+    public GameObject m_PauseMenu;
+
+    //when pause is active
+    bool m_pause = false;
+
+	
+	void Start () {
+
+        m_PauseMenu.SetActive(m_pause);
+	}
+	
+	
+	void Update () {
+	
+        //when user press esc - show menu
+        if (Input.GetKeyDown("escape"))
+        {
+            m_pause = !m_pause;
+            m_PauseMenu.SetActive(m_pause);
+        }
+	}
+}
