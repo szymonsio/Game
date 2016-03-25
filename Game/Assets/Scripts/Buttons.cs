3f296e5a8828c7cdb41d25b66a0deac8f981eab4
szymonsio <szymonsio@gmail.com>
2016-03-25 14:22:34 +0100

    Font + Menu
    Added Segoe Font and enabled some buttons in menu

--- Game/Assets/Scripts/Buttons.cs
+++ Game/Assets/Scripts/Buttons.cs
@@ -7,4 +7,25 @@ public class Buttons : MonoBehaviour {
     {
         SceneManager.LoadScene(0);
     }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void AuthorsButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void CompendiumButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void GameButton()
+    {
+        SceneManager.LoadScene(3);
+    }
 }
+   
